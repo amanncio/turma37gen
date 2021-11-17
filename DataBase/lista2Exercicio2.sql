6e5e84024a9e85bcc15891fc62f6102c52359d34
Matheus Amâncio <matheus.amancio01@gmail.com>
2021-11-17 14:37:29 -0300

    Primeira aplicação Spring + exercicio e task do PI

---
+++ DataBase/lista2Exercicio2.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_pizzaria_legal;
+
+USE db_pizzaria_legal;
+
+CREATE TABLE tb_categoria(
+id_categoria INT AUTO_INCREMENT,
+tipo VARCHAR(255) NOT NULL,
+tamanho VARCHAR(255) NOT NULL,
+
+
+);
